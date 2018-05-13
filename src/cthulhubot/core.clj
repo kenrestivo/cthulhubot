c47de4fc6d546b4000f0c4dd358abd806c2c81f9
ken restivo <ken@restivo.org>
2018-05-12 23:11:42 -0700

    Add proto-docs

--- src/cthulhubot/core.clj
+++ src/cthulhubot/core.clj
@@ -2,6 +2,20 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  ;; TODO: read config and run the sync
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(comment
+
+  ;;; XXX example for what main will need to do
+  (def running
+    (future
+      (bot/run-sync (bot/toke "resources/cthulhu.edn")
+                    "https://matrix.spaz.org"
+                    60000
+                    "s48978_284186_3902_48241_32_21_246_1335"
+                    bot/process-stream!)))
+
+  )
